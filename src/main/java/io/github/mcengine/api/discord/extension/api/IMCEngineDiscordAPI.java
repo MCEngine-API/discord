47bb6774c6e23ed636a7a8be3dc7e7140328a3b8
JetsadaWijit <jetsadawijit@outlook.com>
2025-08-18 04:24:04 +0700

    feat: Create extensions interface

---
+++ src/main/java/io/github/mcengine/api/discord/extension/api/IMCEngineDiscordAPI.java
@@ -0,0 +1,34 @@
+package io.github.mcengine.api.discord.extension.api;
+
+import org.bukkit.plugin.Plugin;
+
+/**
+ * Represents a Discord-based API module that can be dynamically loaded into the MCEngine.
+ * <p>
+ * Implement this interface to expose discord-related APIs to the engine and other modules. Use this to build Discord integrations such as commands, events, and webhooks.
+ */
+public interface IMCEngineDiscordAPI {
+
+    /**
+     * Called when the Discord {module} is loaded by the engine.
+     *
+     * @param plugin The plugin instance providing context.
+     */
+    void onLoad(Plugin plugin);
+
+    /**
+     * Called when the Discord {module} is unloaded or disabled by the engine.
+     * <p>
+     * Use this method to release resources, unregister listeners, or perform any necessary cleanup.
+     *
+     * @param plugin The plugin instance providing context.
+     */
+    void onDisload(Plugin plugin);
+
+    /**
+     * Sets a unique ID for this Discord {module} instance.
+     *
+     * @param id The unique ID assigned by the engine.
+     */
+    void setId(String id);
+}
